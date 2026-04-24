84bb591340a69d3079d15bf95447a7cc6dd68d70
roboaltic <roboaltic19@gmail.com>
2026-04-24 15:05:45 +0300

    Update setup.py

--- phase2/ros2_ws/setup/setup.py
+++ phase2/ros2_ws/setup/setup.py
@@ -1,18 +1,37 @@
 from setuptools import setup
+from glob import glob
+import os
 
-package_name = 'ld06_driver'
+package_name = 'robot_bringup'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')
+        ),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    description='LD06 LiDAR driver',
+    maintainer='Artem',
+    maintainer_email='artem@example.com',
+    description='Robot bringup with lidar obstacle avoidance',
+    license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'ld06_node = ld06_driver.ld06_node:main',
+            'lidar_obstacle_node = robot_bringup.lidar_obstacle_node:main',
+            'ros_stream = robot_bringup.ros_stream:main',
         ],
     },
 )
